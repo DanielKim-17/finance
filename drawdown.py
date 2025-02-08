09e504ce744db214276bc50016e84e7ed9e8e710
DanielKim-17 <mildfogkim@gmail.com>
2025-02-08 17:50:42 +0900

    Add files via upload

--- drawdown.py
+++ drawdown.py
@@ -3,7 +3,8 @@
 # 패키지 불러오기
 import pandas as pd
 import numpy as np
-import yahoo_fin.stock_info as si
+# import yahoo_fin.stock_info as si
+import yfinance as yf
 import math
 import datetime
 import streamlit as st
@@ -41,8 +42,7 @@ if st.button('Get Data'):
     for code in stock_codes:
         if code:  # 주식코드가 입력된 경우에만 처리
             try:
-                df = si.get_data(code, start_date=start_date, end_date=end_date)
-                combined_df[code] = df['close']
+                combined_df[code] = yf.download(code, start=start_date, end=end_date)['Close']
             except Exception as e:
                 st.warning(f"Could not retrieve data for {code}: {e}")
     
@@ -59,4 +59,30 @@ if st.button('Get Data'):
         st.line_chart(drawdown)
     else:
         st.warning("No valid stock data to display.")
+
+
+# # 주식 데이터를 가져오기 및 그래프 그리기
+# if st.button('Get Data'):
+#     combined_df = pd.DataFrame()
+#     for code in stock_codes:
+#         if code:  # 주식코드가 입력된 경우에만 처리
+#             try:
+#                 df = si.get_data(code, start_date=start_date, end_date=end_date)
+#                 combined_df[code] = df['close']
+#             except Exception as e:
+#                 st.warning(f"Could not retrieve data for {code}: {e}")
+    
+#     if not combined_df.empty:
+#         # drawdown 계산
+#         drawdown = drawdown_df(combined_df)
+        
+#         # 주식 그래프 그리기
+#         st.subheader('Stock Prices')
+#         st.line_chart(combined_df)
+        
+#         # drawdown 그래프 그리기
+#         st.subheader('Drawdown')
+#         st.line_chart(drawdown)
+#     else:
+#         st.warning("No valid stock data to display.")
 
