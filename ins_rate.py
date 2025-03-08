e7a4f09f25b131eda747e9df7c06f9ce73527da2
DanielKim-17 <mildfogkim@gmail.com>
2025-03-08 15:20:41 +0900

    Add files via upload

--- ins_rate.py
+++ ins_rate.py
@@ -27,6 +27,8 @@ from fredapi import Fred
 import streamlit as st
 import datetime
 
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
 api_key=st.secrets["fred_api_key"]
 fred = Fred(api_key=st.secrets["fred_api_key"])
 
