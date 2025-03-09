4a3385aff9e1e37bc281c219e4a02b7795cdcd26
DanielKim-17 <mildfogkim@gmail.com>
2025-03-09 10:44:19 +0900

    Add files via upload

--- drawdown.py
+++ drawdown.py
@@ -1,14 +1,10 @@
-# 이 파일은 주식코드와 기간을 받으면 Drawdown을 그려주는 프로그램 입니다
-
-
 # 패키지 불러오기
 import pandas as pd
-import numpy as np
 # import yahoo_fin.stock_info as si
 import yfinance as yf
-import math
 import datetime
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
@@ -37,87 +33,83 @@ with st.sidebar:
     # 여러 주식코드를 입력받기
     stock_codes = [stock_code1, stock_code2, stock_code3]
 
-    start_date = st.date_input('Start Date', datetime.date(2020, 1, 1))
+    start_date = st.date_input('Start Date', datetime.date(2024, 1, 1))
     end_date = st.date_input('End Date', datetime.date.today())
     Button_AZ = st.button('Get Data')
 
-# 주식 데이터를 가져오기 및 그래프 그리기
+# if 'combined_df' not in st.session_state:
+#     st.session_state.combined_df = None
+
+combined_df = pd.DataFrame()
 if Button_AZ:
-    combined_df = pd.DataFrame()
     for code in stock_codes:
         if code:  # 주식코드가 입력된 경우에만 처리
             try:
                 combined_df[code] = yf.download(code, start=start_date, end=end_date)['Close']
             except Exception as e:
                 st.warning(f"Could not retrieve data for {code}: {e}")
+    # st.session_state.combined_df = combined_df
+
+
+# 주식 데이터를 가져오기 및 그래프 그리기
+
+
+if not combined_df.empty:
     combined_df_normalized = (combined_df / combined_df.iloc[0] * 100).round(2)
+else:
+    combined_df_normalized = pd.DataFrame()
+
+drawdown = drawdown_df(combined_df)
 
 
+
+if not combined_df.empty:
+    # drawdown 계산
+    min_drawdown = drawdown.min().round(2) * 100
+    min_drawdown_dates = drawdown.idxmin()
+    latest_date = drawdown.index[-1]
+    latest_drawdown = drawdown.loc[latest_date].round(2) * 100
+
+    min_drawdown_data = {
+    'Market': [],
+    'Min': [],
+    'Min Date': [],
+    'Latest': [],
+    'Latest Date': []
+    }
+    for col in drawdown.columns:
+        min_drawdown_data['Market'].append(col)
+        min_drawdown_data['Min'].append(min_drawdown[col])
+        min_drawdown_data['Min Date'].append(min_drawdown_dates[col].date())
+        min_drawdown_data['Latest'].append(latest_drawdown[col])
+        min_drawdown_data['Latest Date'].append(latest_date.date())
     
-    if not combined_df.empty:
-        # drawdown 계산
-        drawdown = drawdown_df(combined_df)
+    # 주식 그래프 그리기
+    st.subheader('Stock Prices')
+    fig = px.line(combined_df)
+    fig.update_xaxes(rangeslider_visible=True)
+    st.plotly_chart(fig)
 
 
-        min_drawdown = drawdown.min().round(2) * 100
-        min_drawdown_dates = drawdown.idxmin()
-        latest_date = drawdown.index[-1]
-        latest_drawdown = drawdown.loc[latest_date].round(2) * 100
 
-        min_drawdown_data = {
-        'Market': [],
-        'Min': [],
-        'Min Date': [],
-        'Latest': [],
-        'Latest Date': []
-        }
-        for col in drawdown.columns:
-            min_drawdown_data['Market'].append(col)
-            min_drawdown_data['Min'].append(min_drawdown[col])
-            min_drawdown_data['Min Date'].append(min_drawdown_dates[col].date())
-            min_drawdown_data['Latest'].append(latest_drawdown[col])
-            min_drawdown_data['Latest Date'].append(latest_date.date())
-        
-        # 주식 그래프 그리기
-        st.subheader('Stock Prices')
-        st.line_chart(combined_df)
-        
-        st.subheader('Stock Prices Normalized')
-        st.line_chart(combined_df_normalized)
-
-        # drawdown 그래프 그리기
-        st.subheader('Drawdown')
-        st.line_chart(drawdown)
-
-        st.subheader('Minimum Drawdown by Market')
-        st.dataframe(min_drawdown_data, hide_index = True)
-        
-    else:
-        st.warning("No valid stock data to display.")
-
-
-# # 주식 데이터를 가져오기 및 그래프 그리기
-# if st.button('Get Data'):
-#     combined_df = pd.DataFrame()
-#     for code in stock_codes:
-#         if code:  # 주식코드가 입력된 경우에만 처리
-#             try:
-#                 df = si.get_data(code, start_date=start_date, end_date=end_date)
-#                 combined_df[code] = df['close']
-#             except Exception as e:
-#                 st.warning(f"Could not retrieve data for {code}: {e}")
+    # st.line_chart(combined_df)
     
-#     if not combined_df.empty:
-#         # drawdown 계산
-#         drawdown = drawdown_df(combined_df)
-        
-#         # 주식 그래프 그리기
-#         st.subheader('Stock Prices')
-#         st.line_chart(combined_df)
-        
-#         # drawdown 그래프 그리기
-#         st.subheader('Drawdown')
-#         st.line_chart(drawdown)
-#     else:
-#         st.warning("No valid stock data to display.")
+    st.subheader('Stock Prices Normalized')
+    fig = px.line(combined_df_normalized)
+    fig.update_xaxes(rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+
+    # drawdown 그래프 그리기
+    st.subheader('Drawdown')
+    fig = px.line(drawdown)
+    fig.update_xaxes(rangeslider_visible=True)
+    st.plotly_chart(fig)    
+
+    st.subheader('Minimum Drawdown by Market')
+    st.dataframe(min_drawdown_data, hide_index = True)
+    
+else:
+    st.warning("No valid stock data to display.")
+
 
